8a1027155dfba05a70d983185871ad8b995c3235
Layer-norm <magicianaio@gmail.com>
2026-02-04 13:15:23 +0800

    pkg init

--- src/rgba_vae/__init__.py
+++ src/rgba_vae/__init__.py
@@ -1,2 +1,17 @@
-def main() -> None:
-    print("Hello from rgba-vae!")
+"""
+RGBA Variational Autoencoder (RGBA-VAE)
+
+Author: Faxuan Cai
+
+License: MIT License
+"""
+
+from .train import train_vae
+from .vae import VAE
+from .defaultconfig import DefaultConfig
+
+__all__ = [
+    "train_vae",
+    "VAE",
+    "DefaultConfig"
+]
