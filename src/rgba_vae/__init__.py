e430f3a951a775b5321297dc2a3d141e13d692f1
Layer-norm <magicianaio@gmail.com>
2026-02-05 12:31:44 +0800

    handle base64 strintg

--- src/rgba_vae/__init__.py
+++ src/rgba_vae/__init__.py
@@ -9,9 +9,11 @@ License: MIT License
 from .train import train_vae
 from .vae import VAE
 from .defaultconfig import DefaultConfig
+from .utils import extract_base64_image_data
 
 __all__ = [
     "train_vae",
     "VAE",
-    "DefaultConfig"
+    "DefaultConfig",
+    "extract_base64_image_data"
 ]
---
+++ src/rgba_vae/utils.py
@@ -0,0 +1,29 @@
+# handle base64 encoded image strings
+import torch
+from torchvision import transforms as T
+
+import io
+import base64
+
+from PIL import Image
+
+from typing import List, Dict
+
+def extract_base64_image_data(image_str: str) -> torch.Tensor:
+    """
+    Extract image data from a base64 encoded string.
+    """
+    if "base64" in image_str:
+        image_str = image_str.split(",")[1]
+
+    image_data = base64.b64decode(image_str)
+
+    image = Image.open(io.BytesIO(image_data)).convert("RGBA")
+
+    transform = T.Compose([
+        T.Resize((64, 64)),
+        T.ToTensor(),
+        T.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))
+    ])
+
+    return transform(image)
