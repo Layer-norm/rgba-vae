08c35b56f16052291c179914c1ca69c2d8349af6
Layer-norm <magicianaio@gmail.com>
2026-02-15 15:51:14 +0800

    refactor vae

--- src/rgba_vae/vae.py
+++ src/rgba_vae/vae.py
@@ -23,15 +23,20 @@ class DoubleConv(nn.Module):
         return self.double_conv(x)
 
 
-class VAE(nn.Module):
-    def __init__(self, in_channels: int, image_size: int, hidden_dims: List[int], latent_dim: int, dropout: float = 0.1) -> None:
+# Encoder: q(z|x)
+class Encoder(nn.modules):
+    def __init__(self, 
+                 in_channels: int, 
+                 image_size: int, 
+                 hidden_dims: List[int], 
+                 latent_dim:int, 
+                 dropout: float = 0.1
+        ) -> None:
         super().__init__()
         self.in_channels = in_channels
-        self.image_size = image_size
         self.hidden_dims = hidden_dims
         self.hidden_dim = hidden_dims[-1]
         self.latent_dim = latent_dim
-
 
         # encoder
         self.encoder_layers = nn.ModuleList()
@@ -41,26 +46,14 @@ class VAE(nn.Module):
             self.encoder_layers.append(DoubleConv(input_dim, h_dim, dropout=dropout))
             self.encoder_layers.append(nn.MaxPool2d(2))
             input_dim = h_dim
-
+        
         final_feature_map_size = image_size // (2 ** len(hidden_dims))
         self.flattened_dim = int(hidden_dims[-1] * (final_feature_map_size ** 2))
 
         self.fc_mu = nn.Linear(self.flattened_dim, latent_dim)
         self.fc_log_var = nn.Linear(self.flattened_dim, latent_dim)
-
-        # decoder
-        self.fc_decoder = nn.Linear(latent_dim, self.flattened_dim)
-        reversed_hidden_dims = hidden_dims[::-1] + [in_channels]
-        self.decoder_layers = nn.ModuleList()
-        first_decoder_dim = reversed_hidden_dims[0]
-
-        for h_dim in reversed_hidden_dims[1:]:
-            self.decoder_layers.append(DoubleConv(first_decoder_dim, h_dim, dropout=dropout))
-            self.decoder_layers.append(nn.Upsample(scale_factor=2, mode='nearest'))
-            first_decoder_dim = h_dim
-
     
-    def encode(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
 
         for layer in self.encoder_layers:
             x = layer(x)
@@ -70,15 +63,63 @@ class VAE(nn.Module):
         log_var = self.fc_log_var(x)
         return mu, log_var
 
-    def decode(self, x: torch.Tensor) -> torch.Tensor:
+
+# Decoder/Generator: p(x|z)
+class Decoder(nn.modules):
+    def __init__(self, 
+                 in_channels: int, 
+                 image_size: int, 
+                 hidden_dims: List[int], 
+                 latent_dim: int, 
+                 dropout: float = 0.1
+        ) -> None:
+
+        self.feature_map_size = image_size // (2 ** len(hidden_dims))
+        self.flattened_dim = int(hidden_dims[-1] * (self.feature_map_size ** 2))
+        reversed_hidden_dims = hidden_dims[::-1] + [in_channels]
+
+        #decoder
+        self.fc_decoder = nn.Linear(latent_dim, self.flattened_dim)
+        self.decoder_layers = nn.ModuleList()
+        decoder_input_dim = reversed_hidden_dims[0]
+
+        for h_dim in reversed_hidden_dims[1:]:
+            self.decoder_layers.append(DoubleConv(decoder_input_dim, h_dim, dropout=dropout))
+            self.decoder_layers.append(nn.Upsample(scale_factor=2, mode='nearest'))
+            decoder_input_dim = h_dim
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.fc_decoder(x)
-        feature_map_size = self.image_size // (2 ** len(self.hidden_dims))
-        x = x.view(-1, self.hidden_dim, feature_map_size, feature_map_size)
+
+        x = x.view(-1, self.hidden_dim, self.feature_map_size, self.feature_map_size)
 
         for layer in self.decoder_layers:
             x = layer(x)
 
         return torch.sigmoid(x)
+
+
+class VAE(nn.Module):
+    def __init__(self, 
+                 encoder:Encoder, 
+                 decoder:Decoder,
+                 in_channels: int, 
+                 image_size: int, 
+                 hidden_dims: List[int], 
+                 latent_dim: int, 
+                 dropout: float = 0.1
+        ) -> None:
+
+        super().__init__()
+        self.in_channels = in_channels
+        self.image_size = image_size
+        self.hidden_dims = hidden_dims
+        self.hidden_dim = hidden_dims[-1]
+        self.latent_dim = latent_dim
+        self.dropout = dropout
+
+        self.encode = encoder(self.in_channels, self.image_size, self.latent_dim, self.dropout)
+        self.decorde = decoder(self.in_channels, self.image_size, self.latent_dim, self.dropout)
     
     def reparameterize(self, mu: torch.Tensor, log_var: torch.Tensor) -> torch.Tensor:
         std = torch.exp(0.5 * log_var)
