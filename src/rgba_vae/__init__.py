8b182de483605bcf9c8ce126bcfa0bf70b98f87c
Layer-norm <magicianaio@gmail.com>
2026-02-20 23:59:10 +0800

    fix init

--- src/rgba_vae/__init__.py
+++ src/rgba_vae/__init__.py
@@ -6,19 +6,23 @@ Author: Faxuan Cai
 License: MIT License
 """
 
-from .train import train_vae, train_vaegan
+from .train import train_vae, train_vaegan, train_vavae
 from .vae import VAE
 from .vaegan import VAEGAN
-from .defaultconfig import DefaultConfig, VAEGANConfig
+from .vavae import VAVAE
+from .defaultconfig import DefaultConfig, VAEGANConfig, VAVAEConfig
 from .utils import extract_base64_image_data, JSONLBase64Dataset
 
 __all__ = [
     "train_vae",
     "train_vaegan",
+    "train_vavae",
     "VAE",
     "VAEGAN",
+    "VAVAE",
     "DefaultConfig",
     "VAEGANConfig",
+    "VAVAEConfig",
     "extract_base64_image_data",
     "JSONLBase64Dataset"
 ]
